71e885c3dafcda6c5625d056ef83fb1b6ce7d2b3
timothee-dhenain-zenika <timothee.dhenain@zenika.com>
2023-09-22 17:34:57 -0400

    Secure API key

--- src/app/core/services/fetch-items/fetch-items.service.ts
+++ src/app/core/services/fetch-items/fetch-items.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environment/environment';
 
 import { ItemsDto, RequestParams } from '../../interfaces/movie';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FetchItemsService {
 
-  private URL_API: string = 'http://www.omdbapi.com/?apikey=f8471a10&';
+  private URL_API: string = `http://www.omdbapi.com/?apikey=${environment.omdbApiKey}`;
 
   constructor(private httpClient: HttpClient) {
   }
