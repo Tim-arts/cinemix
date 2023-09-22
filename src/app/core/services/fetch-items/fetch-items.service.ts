b086262a3515428fa0472d2580f50523d8f863a4
timothee-dhenain-zenika <timothee.dhenain@zenika.com>
2023-09-22 17:40:46 -0400

    Fixes

--- src/app/core/services/fetch-items/fetch-items.service.ts
+++ src/app/core/services/fetch-items/fetch-items.service.ts
@@ -2,9 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environment/environment';
-
 import { ItemsDto, RequestParams } from '../../interfaces/movie';
-
 
 @Injectable({
   providedIn: 'root'
