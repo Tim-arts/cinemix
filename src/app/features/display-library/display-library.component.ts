01733ab1562f94990c4b6ab6583a71e6f64decff
timothee-dhenain-zenika <timothee.dhenain@zenika.com>
2023-10-13 20:04:26 -0400

    Simplify display

--- src/app/features/display-library/display-library.component.ts
+++ src/app/features/display-library/display-library.component.ts
@@ -9,17 +9,13 @@ import { ItemService } from '../../core/services/item/item.service';
 })
 export class DisplayLibraryComponent implements OnInit {
 
-  private readonly maxItemsPerRow: number = 10;
-
   items!: ItemDto[] | undefined;
-  placeholderItems!: ItemDto[];
 
   constructor(private readonly itemService: ItemService) {
   }
 
   ngOnInit(): void {
     this.items = this.itemService.getItems();
-    this.placeholderItems = new Array(this.maxItemsPerRow - this.items.length);
   }
 
 }
